685b824cb58be68ea763edc4b694ba5e85b6c944
valparajara <valeriaparajara@gmail.com>
2017-06-21 10:48:02 -0300

    Install bootstrap

--- application/views/home.php
+++ application/views/home.php
@@ -3,6 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 ?>
 <html>
-<head></head>
-<body>OLá Mundo</body>
+<head>
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+
+</head>
+<body>
+
+
+
+
+<div class="well" style="width:50%; margin: 0 auto;">
+
+	<h3>Olá Mundo!</h3>
+
+</div>
+
+<script type="text/javascript" src="<?php echo base_url("assets/js/jQuery-1.10.2.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+
+</body>
 </html>
